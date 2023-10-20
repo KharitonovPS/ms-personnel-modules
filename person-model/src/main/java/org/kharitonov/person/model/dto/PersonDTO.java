6e7a9a34d9d6fb7c25e85982fb85c930416b46c9
KharitonovPS <pasyaoo@gmail.com>
2023-10-20 22:52:13 +0300

    add validation to dto

--- person-model/src/main/java/org/kharitonov/person/model/dto/PersonDTO.java
+++ person-model/src/main/java/org/kharitonov/person/model/dto/PersonDTO.java
@@ -1,5 +1,9 @@
 package org.kharitonov.person.model.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,7 +14,11 @@ import lombok.ToString;
 @ToString
 public class PersonDTO {
 
+    @NotEmpty(message = "Name should not be empty!")
+    @Size(min = 2, max = 30, message = "Name length should be between 2 and 30 characters")
     private String name;
 
+    @Min(value = 0, message = "Age should be greater than 0")
+    @Max(value = 150, message = "Age must be less than or equal to 150")
     private int age;
 }
