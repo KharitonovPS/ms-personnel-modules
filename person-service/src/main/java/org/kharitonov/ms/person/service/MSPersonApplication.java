ace705c695acf671cfad28df32567f20f89e87ae
KharitonovPS <pasyaoo@gmail.com>
2023-10-22 00:54:56 +0300

    enable logging in LoadDatabase with lombok annotation

--- person-service/src/main/java/org/kharitonov/ms/person/service/MSPersonApplication.java
+++ person-service/src/main/java/org/kharitonov/ms/person/service/MSPersonApplication.java
@@ -3,22 +3,14 @@ package org.kharitonov.ms.person.service;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
-import org.kharitonov.ms.person.service.util.BindingResultMessageBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.validation.BindingResult;
 
 @SpringBootApplication
 public class MSPersonApplication {
     public static void main(String[] args) {
         SpringApplication.run(MSPersonApplication.class, args);
-    }
-
-
-    @Bean
-    public BindingResultMessageBuilder bindingResultMessageBuilder(){
-        return new BindingResultMessageBuilder();
     }
 
     @Bean
