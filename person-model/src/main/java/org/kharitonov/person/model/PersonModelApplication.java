03b43923a4ac13437a38ae040052c90259a7f38f
KharitonovPS <pasyaoo@gmail.com>
2023-10-23 00:00:10 +0300

    remove PersonModelApplication

--- person-model/src/main/java/org/kharitonov/person/model/PersonModelApplication.java
+++
@@ -1,6 +0,0 @@
-package org.kharitonov.person.model;
-
-public class PersonModelApplication {
-    public static void main(String[] args) {
-    }
-}
--- person-service/src/main/java/org/kharitonov/ms/person/service/util/PersonNotCreatedException.java
+++
@@ -1,7 +0,0 @@
-package org.kharitonov.ms.person.service.util;
-
-public class PersonNotCreatedException extends RuntimeException {
-    public PersonNotCreatedException(String message){
-        super(message);
-    }
-}
