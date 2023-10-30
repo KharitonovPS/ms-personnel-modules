b1bf630bcb7ab4605d88520e49b94aea2a476bbe
KharitonovPS <pasyaoo@gmail.com>
2023-10-30 23:51:14 +0300

    create CustomPageImpl.java to deserialize response from /persons endpoint

---
+++ person-client/src/main/java/org/kharitonov/person/http/client/util/CustomPageImpl.java
@@ -0,0 +1,40 @@
+package org.kharitonov.person.http.client.util;
+
+        import com.fasterxml.jackson.annotation.JsonCreator;
+        import com.fasterxml.jackson.annotation.JsonIgnore;
+        import com.fasterxml.jackson.annotation.JsonProperty;
+        import com.fasterxml.jackson.databind.JsonNode;
+        import org.springframework.data.domain.PageImpl;
+        import org.springframework.data.domain.PageRequest;
+        import org.springframework.data.domain.Pageable;
+
+        import java.util.ArrayList;
+        import java.util.List;
+
+public class CustomPageImpl<T> extends PageImpl<T> {
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public CustomPageImpl(@JsonProperty("content") List<T> content,
+                          @JsonProperty("first") boolean first,
+                          @JsonProperty("number") int number,
+                          @JsonProperty("size") int size,
+                          @JsonProperty ("totalElements") Long totalElements,
+                          @JsonProperty("pageable") JsonNode pageable, @JsonProperty("last") boolean last,
+                          @JsonProperty("empty") boolean empty,
+                          @JsonProperty("totalPages") int totalPages, @JsonProperty("sort") JsonNode sort,
+                          @JsonProperty("numberOfElements") int numberOfElements) {
+        super(content, PageRequest.of(number, 1), 10);
+    }
+
+    public CustomPageImpl(List<T> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public CustomPageImpl(List<T> content) {
+        super(content);
+    }
+
+    public CustomPageImpl() {
+        super(new ArrayList<>());
+    }
+}
