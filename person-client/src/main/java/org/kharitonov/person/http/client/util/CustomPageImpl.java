f4389d6b989450b49cf7481240efe3c9cb132978
KharitonovPS <pasyaoo@gmail.com>
2023-10-31 23:57:30 +0300

    remove main class from person-model module

--- person-client/src/main/java/org/kharitonov/person/http/client/util/CustomPageImpl.java
+++ person-client/src/main/java/org/kharitonov/person/http/client/util/CustomPageImpl.java
@@ -1,15 +1,14 @@
 package org.kharitonov.person.http.client.util;
 
-        import com.fasterxml.jackson.annotation.JsonCreator;
-        import com.fasterxml.jackson.annotation.JsonIgnore;
-        import com.fasterxml.jackson.annotation.JsonProperty;
-        import com.fasterxml.jackson.databind.JsonNode;
-        import org.springframework.data.domain.PageImpl;
-        import org.springframework.data.domain.PageRequest;
-        import org.springframework.data.domain.Pageable;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
-        import java.util.ArrayList;
-        import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomPageImpl<T> extends PageImpl<T> {
 
--- person-model/src/main/java/org/kharitonov/person/model/dto/ModelMainClass.java
+++
@@ -1,7 +0,0 @@
-package org.kharitonov.person.model.dto;
-
-public class ModelMainClass {
-    public static void main(String[] args) {
-
-    }
-}
