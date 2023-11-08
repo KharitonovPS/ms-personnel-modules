64ec37981f1d4e1e7fb4418c422f5cf9790acb07
KharitonovPS <pasyaoo@gmail.com>
2023-11-08 23:57:12 +0300

    rework batching to personController save() method

--- person-service/src/main/java/org/kharitonov/ms/person/service/service/PersonService.java
+++ person-service/src/main/java/org/kharitonov/ms/person/service/service/PersonService.java
@@ -10,12 +10,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -31,19 +28,11 @@ public class PersonService {
     }
 
     public void save() {
-        List<Person> resultList = new ArrayList<>();
-        int batchSize = 50;
-        if (queueService.getPersonDTOQueue().size() >= batchSize) {
-            while (!queueService.isEmpty()) {
-                resultList = queueService.getPersonDTOQueue()
+        List<Person> personList = queueService.getPersonsFromQueue()
                         .stream()
-                        .map(person -> queueService.getFromQueue())
                         .map(personDTOMapper::dtoToPerson)
-                        .collect(Collectors.toList());
-            }
-            personRepo.saveAllAndFlush(resultList);
-            resultList.clear();
-        }
+                        .toList();
+            personRepo.saveAllAndFlush(personList);
     }
 
     public void updatePerson(Long id, PersonDTO personDTO) {
