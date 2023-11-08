cd80081a5931139ed0fcd48efec92e443f9090d7
KharitonovPS <pasyaoo@gmail.com>
2023-11-08 17:58:08 +0300

    rework QueueService

--- person-service/src/main/java/org/kharitonov/ms/person/service/service/QueueService.java
+++ person-service/src/main/java/org/kharitonov/ms/person/service/service/QueueService.java
@@ -1,5 +1,7 @@
 package org.kharitonov.ms.person.service.service;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.kharitonov.person.model.dto.PersonDTO;
 import org.springframework.stereotype.Service;
 
@@ -7,18 +9,21 @@ import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Service
+@RequiredArgsConstructor
+@Getter
 public class QueueService {
-    
+
     private final Queue<PersonDTO> personDTOS = new ConcurrentLinkedQueue<>();
-    public void addToQueue(PersonDTO personDTO){
+
+    public void addToQueue(PersonDTO personDTO) {
         personDTOS.add(personDTO);
     }
-    
-    public PersonDTO getFromQueue(){
+
+    public PersonDTO getFromQueue() {
         return personDTOS.remove();
     }
 
-    boolean isEmpty(){
+    public boolean isEmpty() {
         return personDTOS.isEmpty();
     }
 
