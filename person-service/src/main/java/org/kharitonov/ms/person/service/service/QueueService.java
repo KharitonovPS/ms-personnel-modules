cdd0d8924918f4e1fc4321d07d0c42d503ed3598
KharitonovPS <pasyaoo@gmail.com>
2023-11-12 23:52:49 +0300

    remove Mapping, add flags private final iS_NOT_EMPTY ;

--- person-service/src/main/java/org/kharitonov/ms/person/service/service/QueueService.java
+++ person-service/src/main/java/org/kharitonov/ms/person/service/service/QueueService.java
@@ -2,6 +2,7 @@ package org.kharitonov.ms.person.service.service;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.kharitonov.ms.person.service.mapper.PersonDTOMapper;
 import org.kharitonov.person.model.dto.PersonDTO;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +15,32 @@ import java.util.concurrent.LinkedBlockingQueue;
 @Getter
 public class QueueService {
 
+    private final PersonDTOMapper personDTOMapper;
+
     private final LinkedBlockingQueue<PersonDTO> personQueue = new LinkedBlockingQueue<>();
 
     public void addToQueue(PersonDTO personDTO) {
         personQueue.add(personDTO);
     }
 
+    private final Object IS_NOT_EMPTY = new Object();
+
     public List<PersonDTO> getPersonsFromQueue() {
         List<PersonDTO> resultList = new ArrayList<>();
         personQueue.drainTo(resultList, 10);
         return resultList;
     }
+
+    public void notifyIsNotEmpty() {
+        synchronized (IS_NOT_EMPTY) {
+            IS_NOT_EMPTY.notify();
+        }
+    }
+
+    public void waitIsNotEmpty() throws InterruptedException {
+        synchronized (IS_NOT_EMPTY) {
+            IS_NOT_EMPTY.wait();
+        }
+    }
+
 }
